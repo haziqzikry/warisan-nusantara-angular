af8efcecdf99fbdc5a54212cfebc14fdf1ade342
Haziq Zikry <haziqzikry@gmail.com>
2023-06-11 08:24:10 +0800

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,29 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { DemoComponent } from './demo/demo.component';
+import { GetUsersComponent } from './get-users/get-users.component';
+import { GetUserComponent } from './get-user/get-user.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { AdminComponent } from './admin/admin.component';
+import { CollectionComponent } from './collection/collection.component';
+import { AppMainNavComponent } from './app-main-nav/app-main-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarisanNusantaraComponent
+    WarisanNusantaraComponent,
+    DemoComponent,
+    GetUsersComponent,
+    GetUserComponent,
+    AddUserComponent,
+    UpdateUserComponent,
+    DeleteUserComponent,
+    AdminComponent,
+    CollectionComponent,
+    AppMainNavComponent
   ],
   imports: [
     BrowserModule,
